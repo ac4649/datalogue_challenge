eeecf86c4da93454582e2fdfe0e834bfce913ecf
Adrien Cogny <ac2753@cornell.edu>
2018-04-17 22:18:18 -0400

    Added descriptions of data, starting to create model

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 import pandas as pd
+import dynet as dy
 
-
-
+# ----------------------------- DATA LOADING ----------------------------- #
 # load the data from the file
 data = pd.read_csv('deepnlp/Sheet_1.csv')
+
 # remove extraneous columsn from the dataframe
 data = data.drop(['Unnamed: 3','Unnamed: 4','Unnamed: 5','Unnamed: 6','Unnamed: 7'],axis=1)
+
+
+# ----------------------------- DATA DESCRIPTION ----------------------------- #
+# column: response_id
+# description: id of the response (we have 80 unique ids)
+
+# column: class
+# description: whether the response_text was flagged for referal to a specialist 
+#               (we have 2 unique values: not_flagged or flagged)
+
+# column: response_text
+# description: natural language response a user inputed to the chat bot, 
+#               must be classified as flagged for assistance or not_flagged.
+
+
+# looking at statistics for the class column in the dataset to see if it is a roughly even dataset 
+print(data['class'].describe())
+# the fact that the frequency of the not_flagged is 55 over the 80 shows a slight
+#  imbalance toward the not_flagged class.
+
+# ------------------- SEPARATION INTO TRAIN AND TEST SPLITS -------------------#
 
 # train on a certain percentage of the data
 percentTrain = 0.6
@@ -15,6 +37,5 @@ data_train = data.sample(frac=percentTrain)
 # the rest of the data will be used as dev
 data_dev = data.drop(data_train.index)
 
+# ------------------- CREATION OF MODEL -------------------#
 
-
-print(data.head())
