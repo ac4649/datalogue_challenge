55e22264903093f008dcc0f0571f1a333820b08a
Adrien Cogny <ac2753@cornell.edu>
2018-04-21 18:28:20 -0400

    working on report and re-running rnn tests.

--- main.py
+++ main.py
@@ -124,13 +124,13 @@ def runRNNModel(hidden_dim = 4, num_layers = 1, embedding_size = 200, embedding_
 def runRNNTrials():
     # Run the model multiple times with a given set of parameters to get the best parameters on average 
     # (no matter what the training )
-    numRuns = 10
-    for j in tqdm([500],desc='Param Variation'):
+    numRuns = 15
+    for j in tqdm([50,100,200,300],desc='Param Variation'):
     # for j in trange(0,10,desc='Param Variation'):
         overallModelStats = pd.DataFrame(index=[i for i in range(numRuns)],columns=['maxEpochs','num_layers','embeddingSize','hiddenDim','train_loss','accuracy','truePos','trueNeg','falsePos','falseNeg'])
         for i in trange(0,numRuns,desc="Param Runs "):
             # print("Run: " +str(i))
-            preds, stats, params = runRNNModel(hidden_dim = 3, num_layers = 1, embedding_size = 300, embedding_file = 'glove/glove.6B.300d.txt',maxEpochs = j)
+            preds, stats, params = runRNNModel(hidden_dim = 3, num_layers = 1, embedding_size = j, embedding_file = 'glove/glove.6B.'+str(j)+'d.txt',maxEpochs = 400)
             # overallModelStats.append((stats,params))
             # curRun = pd.Series([param for param in params]+[stats for i in stats])
             overallModelStats.iloc[i]['maxEpochs'] = params[0]
@@ -144,7 +144,7 @@ def runRNNTrials():
             overallModelStats.iloc[i]['trueNeg'] = stats[2]
             overallModelStats.iloc[i]['falsePos'] = stats[3]
             overallModelStats.iloc[i]['falseNeg'] = stats[4]
-        overallModelStats.to_csv('train_maxEpoch_stats'+str(j)+'.csv')
+        overallModelStats.to_csv('embedd_size_stats'+str(j)+'.csv')
 
     return overallModelStats
 
@@ -165,19 +165,25 @@ def runRandomForestModel(n_estimators = 2):
     # # print(params)
     return stats
 
-numRuns = 15
-randForestStats = pd.DataFrame(index=[i for i in range(numRuns)],columns = ['n_estimators','accuracy','truePos','trueNeg','falsePos','falseNeg'])
+def runRandomForesTrials():
+    numRuns = 15
+    randForestStats = pd.DataFrame(index=[i for i in range(numRuns)],columns = ['n_estimators','accuracy','truePos','trueNeg','falsePos','falseNeg'])
 
-for j in trange(16,21,desc='Changing parameters'):
-    for i in trange(numRuns,desc='Running Models'):
-        curStats = runRandomForestModel(n_estimators = j)
-        randForestStats.iloc[i]['accuracy'] = curStats[0]
-        randForestStats.iloc[i]['truePos'] = curStats[1]
-        randForestStats.iloc[i]['trueNeg'] = curStats[2]
-        randForestStats.iloc[i]['falsePos'] = curStats[3]
-        randForestStats.iloc[i]['falseNeg'] = curStats[4]
-        randForestStats.iloc[i]['n_estimators'] = j
+    for j in trange(16,21,desc='Changing parameters'):
+        for i in trange(numRuns,desc='Running Models'):
+            curStats = runRandomForestModel(n_estimators = j)
+            randForestStats.iloc[i]['accuracy'] = curStats[0]
+            randForestStats.iloc[i]['truePos'] = curStats[1]
+            randForestStats.iloc[i]['trueNeg'] = curStats[2]
+            randForestStats.iloc[i]['falsePos'] = curStats[3]
+            randForestStats.iloc[i]['falseNeg'] = curStats[4]
+            randForestStats.iloc[i]['n_estimators'] = j
 
-    # print(randForestStats)
-    # print(randForestStats.mean())
-    randForestStats.to_csv('randomForest_meanNotSum_'+str(j)+'_estimators')
+        # print(randForestStats)
+        # print(randForestStats.mean())
+        randForestStats.to_csv('randomForest_meanNotSum_'+str(j)+'_estimators')
+
+
+# runRandomForesTrials()
+
+runRNNTrials()
